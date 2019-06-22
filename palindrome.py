6057d6791b744bef71f2155156396cce4d6dbb9a
sushantmandalshm <sushantmandal.shm@gmail.com>
2019-06-22 18:52:42 +0530

    Test Commit from VSCode

--- palindrome.py
+++ palindrome.py
@@ -6,7 +6,7 @@ def isPalindrome(n):
         if num_str[::-1] == str(n):
             return 1
         else:
-            return 0
+            return 0 
 
 print(isPalindrome(12121))
 print(isPalindrome(2147447412))
